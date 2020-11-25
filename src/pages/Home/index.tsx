7bed201ba195d2542f6a4c23f845c2d1acaac1e9
Bruno Matheus <bmatheusc@gmail.com>
2020-11-24 21:46:34 -0300

    Adding products and quotes boxes to homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 import Button from '../../components/Button';
 import Flex from '../../components/Flex';
 
-import { Container, Basics, Banners, BestSellings, MoreProducts, Quotes, Headline } from './styles';
+import { Container, Carousel, Basics, Banners, BestSellings, MoreProducts, Quotes, Headline, Box, Image, Title, Info, Price, QuotesBox, Client } from './styles';
+import client from '../../assets/client-1.svg';
 
 const Home: FC = () => {
 	return (
@@ -47,15 +48,87 @@ const Home: FC = () => {
 				</Basics>
 
 				<BestSellings>
+					<Flex flexDirection="column">
+						<h3>Best selling products</h3>
+						<Link to="/">Kitchen</Link>
+						<Link to="/">Meat and fish</Link>
+						<Link to="/">Special nutrition</Link>
+						<Link to="/">Pharmacy</Link>
+						<Link to="/">Baby</Link>
 
+						<Flex>
+							<Button>More products</Button>
+						</Flex>
+						
+					</Flex>
+
+				{ [0, 1, 2].map((_, index) => {
+					return (
+						<Flex key={ index }>
+							<Box>
+								<Image></Image>
+								<Title>Product Title</Title>
+								<span>Space for a small product description</span>
+
+								<Info>
+									<Price>1.48 USD</Price>
+									<Button>Buy now</Button>
+								</Info>
+							</Box>
+						</Flex>
+					);
+				})}
 				</BestSellings>
 
 				<MoreProducts>
+					<Flex flexDirection="column">
+						<h3>Best selling products</h3>
+						<Link to="/">Kitchen</Link>
+						<Link to="/">Meat and fish</Link>
+						<Link to="/">Special nutrition</Link>
+						<Link to="/">Pharmacy</Link>
+						<Link to="/">Baby</Link>
 
+						<Flex>
+							<Button>More products</Button>
+						</Flex>
+						
+					</Flex>
+
+				{ [0, 1, 2].map((_, index) => {
+					return (
+						<Flex key={ index }>
+							<Box>
+								<Image></Image>
+								<Title>Product Title</Title>
+								<span>Space for a small product description</span>
+								
+								<Info>
+									<Price>1.48 USD</Price>
+									<Button>Buy now</Button>
+								</Info>
+							</Box>
+						</Flex>
+					);
+				})}
 				</MoreProducts>
 
 				<Quotes>
+					<h2>Our customer says</h2>
 
+					<Carousel>
+					{ [0, 1, 2].map((_, index) => {
+						return (
+							<QuotesBox key={ index }>
+								<span>"This is an super space for your customers qoute. Don’t worry it works smooth as pie. You will get all what you need by writiing a text here"</span>
+
+								<small>Name and Surname</small>
+
+								<Client src={ client } />
+							</QuotesBox>
+						);
+					})}
+					</Carousel>
 				</Quotes>
 
 				<Headline>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,11 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Flex from '../../components/Flex';
 
 import background from '../../assets/background-banner.svg';
 
 export const Container = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
 
 	h3 {
 		font-size: 18px;
@@ -13,7 +13,7 @@ export const Container = styled.div`
 	}
 `;
 
-export const Basics = styled.div`
+const basicDiv = css`
 	display: flex;
 	flex: 1;
 	align-items: center;
@@ -38,6 +38,10 @@ export const Basics = styled.div`
 	}
 `;
 
+export const Basics = styled.div`
+	${basicDiv};
+`;
+
 export const Banners = styled(Flex)`
 	height: 280px;
 	width: 38%;
@@ -56,18 +60,116 @@ export const Banners = styled(Flex)`
 	background-size: cover;
 `;
 
-export const BestSellings = styled.div`
-
+export const BestSellings = styled(Flex)`
+	${basicDiv};
+	margin: 64px 0;
 `;
 
 export const MoreProducts = styled.div`
-
+	${basicDiv};
+	margin: 64px 0;
 `;
 
 export const Quotes = styled.div`
+	display: flex;
+	flex-direction: column;
 
+	margin: 64px 0;
+
+	h2 {
+		margin-bottom: 32px;
+	}
+`;
+
+export const Carousel = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+`;
+
+export const Client = styled.img`
+	width: 48px;
+	height: 48px;
+	border-radius: 50%;
+	margin-bottom: 10px;
+`;
+
+export const QuotesBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-between;
+
+	height: 250px;
+	width: 32%;
+
+	background: #ffffff;
+	border: 1px solid #f1f1f1;
+	box-sizing: border-box;
+	border-radius: 12px;	
+
+	span {
+		font-size: 15px;
+		font-weight: 500;
+		text-align: center;
+
+		padding: 24px 32px;
+	}
 `;
 
 export const Headline = styled.div`
 
+`;
+
+export const Box = styled.div`
+	display: flex;
+	flex-direction: column;
+
+	height: 323px;
+	padding: 16px;
+
+	border: 1px solid #d1d1d1;
+	box-sizing: border-box;
+	border-radius: 12px;
+
+	span {
+		font-size: 12px;
+		font-weight: 400;
+		color: #575757;
+	}
+`;
+
+export const Image = styled.div`
+	height: 180px;
+	background: #f9f9f9;
+	border-radius: 12px;
+`;
+
+export const Title = styled.h2`
+	margin-top: 16px;
+	margin-bottom: 0;
+	font-size: 15px;
+	font-weight: 500;
+`;
+
+export const Info = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	margin-top: 16px;
+
+	h3 {
+		margin: 0;
+	}
+
+	button {
+		margin: 0;
+	}
+`;
+
+
+export const Price = styled.h3`
+	font-size: 18px;
+	font-weight: 600;
 `;
